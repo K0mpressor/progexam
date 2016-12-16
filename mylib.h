f96703f7707c9371e31273b60b02643bada4b199
K0mpressor <tikafos@stromox.com>
2016-12-16 11:32:56 +0100

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -1,1 +1,16 @@
+typedef struct oberon {
+  unsigned short borsa_oro;
+  unsigned short punti_ferita;
+  unsigned short incantesimi;
+  unsigned short pozione_guaritrice; 
+} sOberon;
+
+typedef struct terra {
+  enum tipo_terra {deserto, foresta, palude, villaggio, pianura} eTipo_terra;
+  enum tipo_mostro {nessuno, scheletro, lupo, orco, drago} eTipo_mostro;
+  unsigned short tesoro;
+  terra *terra_successiva;
+} sTerra;
+
+void crea_percorso();
 
