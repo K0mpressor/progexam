e7fc85e285c0ced539b93b5fa71e83986ee7e683
K0mpressor <tikafos@stromox.com>
2016-12-17 13:17:59 +0100

    Update progexam.c

--- progexam.c
+++ progexam.c
@@ -1,39 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "mylib.h"
 
 void main() {
 	int scelta;
-	do
-	{
-		printf("PROGEXAM - MENU PRINCIPALE\n\n");
-		printf("1 - Crea percorso\n");
-		printf("2 - Muovi Oberon\n");
-		printf("3 - Termina gioco\n\n");
+	char conferma;
+	while (1) {
+		printf("PROGEXAM - MENU PRINCIPALE\n\n1 - Crea percorso\n2 - Muovi Oberon\n3 - Termina gioco\n\n");
 		scanf("%d", &scelta);
 		switch (scelta) {
 		case 1:
-			system("clear");
-			printf("Crea percorso\n");
-      //ToDo: implementare funzione crea_percorso
-			pause();
+			system("cls");
+			creaPercorso();
+			system("cls");
 			break;
 		case 2:
-			system("clear");
+			system("cls");
 			printf("Muovi Oberon\n");
-       //ToDo: implementare funzione muovi_Oberon
-			pause();
 			break;
 		case 3:
-			 //ToDo: implementare funzione termina_gioco
+			//ToDo: implementare funzione termina_gioco
+			system("cls");
+			printf("Sei sicuro di uscire? (y/n)\n");
+			scanf(" %c", &conferma);
+			do {
+				switch (conferma) {
+				case 'y':
+					terminaGioco();
+					break;
+				case 'n':
+					break;
+				default:
+					illegalChoice();
+					break;
+				}
+			} while (conferma == 'y' || conferma == 'n');
 			break;
 		default:
-			system("clear");
-			printf("ERRORE\n");
-			printf("Inserire un numero compreso tra 1 e 3!\n");
-			pause();
-			system("clear");
+			illegalChoice();
 			break;
 		}
-	} while (scelta <1 || scelta >3);
+	}
 }
