76d6ceb68b6884b08bc20bac0e1a6d4fdae4500a
az4kos <a.k.zababurin@gmail.com>
2016-12-18 09:51:40 +0100

    Update mylib.c

--- mylib.c
+++ mylib.c
@@ -1,28 +1,35 @@
 #include "mylib.h"
 
+//						
+//						
+
 //Dichiaro le struttire statiche valide solo in mylib.c
-static struct Terra* percorso = NULL;
+static struct sTerra* percorso = NULL;
 
-static struct Terra* ultima_terra = NULL;
+static struct sTerra* ultima_terra = NULL;
 
-//con static al posto di extern mi dice come se non fosse dichiarato 
-extern struct Oberon oberon;
+static struct sOberon oberon;   // NOTSURE "extern" dovrebbe essere static 
 
 
 //Dichiaro le funzioni da richiamare nei sotto menu'
 
 	//Funzioni del sottomenù crea_percorso()
-	static struct Terra *ins_terra(struct Terra* p);
-	static struct Terra *del_terra(struct Terra* p);
-	static void stampa_percorso(struct Terra* p);
-	static struct Terra *chiudi_percorso(struct Terra* p);
+	static struct sTerra *ins_terra(struct sTerra* p);
+	static struct sTerra *del_terra(struct sTerra* p);
+	static void stampa_percorso(struct sTerra* p);
+	static void chiudi_percorso();
 
 	//Funzioni del sottomenù muovi_Oberon()
-	static struct Oberon *avanza(struct Oberon* l);
-	static struct Oberon *prendi_tesoro(struct Oberon* l);
-	static struct Oberon *usa_pozione(struct Oberon* l);
-	static struct Oberon *combatti(struct Oberon* l);
-	static struct Terra *distruggi_terra(struct Terra* p, struct Oberon* l);
+	static struct sOberon *avanza(struct sOberon* l);
+	static struct sOberon *prendi_tesoro(struct sOberon* l);
+	static struct sOberon *usa_pozione(struct sOberon* l);
+	static struct sOberon *combatti(struct sOberon* l);
+	static struct sTerra *distruggi_terra(struct sTerra* p, struct sOberon* l);
+	
+	
+//Dichiaro un pointer che registri l'ultima posizine inserita
+struct sTerra *ptr=NULL;
+	
 
 
 //Funzione sub_main per la creazione del percorso
@@ -36,31 +43,44 @@ void crea_percorso()
 	
 	for ( ; ; )
 	{	
+		//Controllo del programma nel caso sia stata utilizzata la fun chiudi_percorso e uscire se accaduto
 		if (checkExit!=0){
 			return;
 		}
 		
-		
-		printf("\n--------------FUNZIONE CREA PERCORSO--------------");
+		printf("\n			PROGEXAM - SUBMENU CREA PERCORSO\n\n");
 		printf("\n      Cosa vuoi fare?\n\n");
-		printf("1 - Inserire terreno\n");
-		printf("2 - Cancellare terreno\n");
-		printf("3 - Mostra terreno\n");
-		printf("4 - Chiudi percorso\n");
-		printf("\n  > la tua scelta ");
+		printf(" 1 - Inserire terreno\n");
+		printf(" 2 - Cancellare terreno\n");
+		printf(" 3 - Mostra terreno\n");
+		printf(" 4 - Chiudi percorso\n");
 		
+		// CHECK
+		if (percorso==NULL){
+			printf("\n\nvuota");
+		}
+		else{
+			printf("\n\npiena");
+		}
+		printf("\n%x", &percorso);
+		printf("\n%p", percorso);
+		//CHECK
+		
+		printf("\n  > la tua scelta ");
 		scanf("%s", &sScelta_sub);
-		nScelta_sub=atoi(sScelta_sub);
+		
+		//Coneversione del valore string sScelta_sub in int nScelta_sub per controllare che l'utente abbia utilizzato valori validi
+			nScelta_sub=atoi(sScelta_sub);
 		
 		switch (nScelta_sub) {
 			case 1:
 				system("clear");
-				percorso=ins_terra(percorso);
+				ptr=ins_terra(ptr);
 				break;
 				
 			case 2:
 				system("clear");
-				ultima_terra=del_terra(ultima_terra);
+				ptr=del_terra(ptr);
 				break;
 		
 			case 3:
@@ -70,7 +90,7 @@ void crea_percorso()
 		
 			case 4:
 				system("clear");
-				ultima_terra=chiudi_percorso(ultima_terra);
+				chiudi_percorso();
 				break;
 		
 			default:
@@ -78,8 +98,9 @@ void crea_percorso()
 				printf("\a********ERRORE********\n");
 				printf("Inserire un numero compreso tra 1 e 4!\n\n");
 				printf("\n - Premi ENTER per continuare -");
-				while(getchar()!='\n');
-				getchar();
+				while(getchar()!='\n'){
+					getchar();
+				}
 				break;
 			}
 		}
@@ -94,10 +115,18 @@ void muovi_Oberon()
 	static int nScelta_sub;
 	static char sScelta_sub[1];
 	
-	
+	if (percorso==NULL){
+		printf("\a\n		********ERRORE********\n");
+		printf("    Non hai ancora inserito nessun terreno!");
+		printf("\n\n Premi INVIO per ritornare al menu principale...");
+		while(getchar()!='\n');
+			getchar();
+		return;
+		
+	}
 	for ( ; ; )
 	{
-		printf("\n--------------FUNZIONE DI MOVIMENTO OBERON--------------");
+		printf("\n			PROGEXAM - SUBMENU MOVIMENTO OBERON--------------");
 		printf("\nCosa vuoi fare?\n\n");
 		printf("1 - Avanzare\n");
 		printf("2 - Prendere il tesoro\n");
@@ -135,10 +164,10 @@ void muovi_Oberon()
 			default:
 				system("clear");
 				printf("\a********ERRORE********\n");
-				printf("Inserire un numero compreso tra 1 e 4!\n\n");
+				printf("Inserire un numero compreso tra 1 e 5\n\n");
 				printf("\n - Premi ENTER per continuare -");
 				while(getchar()!='\n');
-				getchar();
+					getchar();
 				break;
 		}
 	}
@@ -148,11 +177,13 @@ void muovi_Oberon()
 //Funzione sub_main per terminare il gioco liberando la memoria utilizzata e uscendo
 void termina_gioco()
 {// termina_gioco() - OPEN
-	system("clear");
-	printf("--------------------------");
-	printf("\n          Termino!");
-	printf("\n--------------------------");
-	exit(0);
+		
+		system("clear");
+		printf("--------------------------");
+		printf("\n          Termino!");
+		printf("\n--------------------------");
+		exit(0);
+	
 }
 
 
@@ -161,153 +192,240 @@ void termina_gioco()
 //-------------FUNZIONI SU TERRA-------------
 
 //Funzione di inserimento terra
-struct Terra *ins_terra(struct Terra* p)
+struct sTerra *ins_terra(struct sTerra* p)
 {// *ins_terra() - OPEN
 	
 	//Inizializzo il random
 	time_t t;
 	srand((unsigned) time(&t));
 	
-	short tesoro;
-	struct Terra *terra_successiva;
+	//Definiscoa le variabili temporanee da immettere
+	int temp_tipo_terra=0;
+	int temp_tipo_mostro=0;
 	
+	struct sTerra *pun;
+	
+	//Inserimento valori a p da percorso
 	if(percorso==NULL){
-		
-		//Alloco memoria di dimensione struct Terra a percorso
-		percorso = (struct Terra *)malloc(sizeof(struct Terra));
-		
-		//Al membro terra_successiva dello struct percorso assegno il valore di p
-		percorso->terra_successiva = p;
-		do{
-			system("clear");
-			printf("Che tipo di terra vuoi?\n\n");
-			printf("\n1 - deserto\n2 - foresta\n3 - Palude\n4 - villaggio\n5 - Pianura ");
-			printf("\n > inserire scelta: ");
-			scanf ("%u", &(percorso->terra_successiva->tipo_terra)); 	//terra_successiva->tipo_terra senza int %u   // da prof : &((int)(p->tipo_terra)))
-		}while((p->tipo_terra)<1 || (p->tipo_terra)>5);
 			
-		ultima_terra=p;
+		//Inserimento terra
+		printf("\n Che tipo di terra vuoi?\n");
+		printf("\n   1 - Deserto\n   2 - Foresta\n   3 - Palude\n   4 - Villaggio\n   5 - Pianura\n");
+		printf("\n > inserire scelta: ");
+			
+		scanf("%d", &temp_tipo_terra); 										// da prof : &((int)(p->tipo_terra))
+		printf("\n Hai inserito %d", temp_tipo_terra);
+			
+			
+		//Inserimento mostri
+		printf("\n\n Che tipo di mostro vuoi?\n");
+		printf("\n   1 - Nessuno\n   2 - Scheletro\n   3 - Lupo\n   4 - Orco\n   5 - Drago");
+		printf("\n > inserire scelta: ");
 		
-		p->terra_successiva=NULL;
-		return(0);
+		scanf("%d", &(temp_tipo_mostro)); 										// da prof : &((int)(p->tipo_terra)))
+		printf("%d", temp_tipo_mostro);
+		 
 	}
 	else{
 		
-		//Al membro terra_successiva di ultima_terra alloco memoria di dimensione struct Terra
-		ultima_terra->terra_successiva=(struct Terra *)malloc(sizeof(struct Terra));
-		
-		//Ad ultima terra ora assegno il membro terra_successiva di ultima_terra
-		ultima_terra=ultima_terra->terra_successiva;
-		
-		
-		p = (struct Terra *)malloc(sizeof(struct Terra)); //NOT SURE - (non necessario?)
-			
-		//p->terra_successiva = NULL;              NOT SURE
-		
-		//A terra_successiva imposto il puntatore p per poter lavorare con la prossima Terra	
-		terra_successiva=p;
-		
-		//        NOT AS b4
-			ultima_terra=p;
-		
-		printf("\n\nChe tipo di terra vuoi?");
-		printf("\n1 - deserto\n2 - foresta\n3 - Palude\n4 - villaggio\n5 - Pianura");
+		//Inserimento terra
+		printf("\n Che tipo di terra vuoi?\n");
+		printf("\n   1 - Deserto\n   2 - Foresta\n   3 - Palude\n   4 - Villaggio\n   5 - Pianura\n");
 		printf("\n > inserire scelta: ");
-		scanf ("%u", &((p->terra_successiva->tipo_terra))); //terra_successiva->tipo_terra senza int %u  // da prof : &((int)etc...
-			
-			
-		printf("\n\n\nChe tipo di mostro vuoi?");
-		printf("\n1 - nessuno\n2 - scheletro\n3 - lupo\n4 - orco\n5 - drago");
-		printf("\n > inserire scelta: ");
-		scanf ("%u", &((p->terra_successiva->tipo_mostro))); //terra_successiva->tipo_mostro senza int %u   // da prof : &((int)etc...
 		
-		//ultima_terra=p;
+			
+		scanf("%d", &(temp_tipo_terra)); 										// da prof : &((int)(p->tipo_terra)))
+		printf("%d", temp_tipo_terra);
+		
+		//Inserimento mostri
+		printf("\n\n Che tipo di mostro vuoi?\n");
+		printf("\n   1 - Nessuno\n   2 - Scheletro\n   3 - Lupo\n   4 - Orco\n   5 - Drago");
+		printf("\n > inserire scelta: ");
+		
+		scanf("%d", &(temp_tipo_mostro)); 										// da prof : &((int)(p->tipo_terra)))
+		printf("%d", temp_tipo_mostro);
+	
 	}
-	return(terra_successiva);
+	
+	//Allocazione memoria
+	if(percorso==NULL){
+		
+		//Alloco al puntatore p
+		p=(struct sTerra*)malloc(sizeof(struct sTerra));
+		
+		
+		//Imposto che la terra successiva di p non abbia nessun valore
+		p->terra_successiva=NULL;
+		
+		
+		//Do a tipo_terra di p il valore della vartiabile temporanea
+		p->tipo_terra=temp_tipo_terra;
+		
+		//Do a tipo_mostro di p il valore della vartiabile temporanea
+		p->tipo_mostro=temp_tipo_mostro;
+		
+		//Imposto tesoro come numero random  
+		p->tesoro=rand()%100;
+		
+		
+		//A percorso assegno l'indirizzo dello struct p
+		percorso= p;
+		
+		//Assegno ad ultima terra l'indirizzo di p
+		ultima_terra=p;
+		
+	}
+	else{
+		
+		//Alloco uno spazio 
+		p->terra_successiva=(struct sTerra *)malloc(sizeof(struct sTerra));
+		
+		//Accedo all' elemento nuovo p->terra_successiva tramite il puntatore pun
+		pun=p->terra_successiva;
+		
+		
+		//Do a tipo_terra di p il valore della vartiabile temporanea
+		pun->tipo_terra=temp_tipo_terra;
+		
+		//Do a tipo_mostro di p il valore della vartiabile temporanea
+		pun->tipo_mostro=temp_tipo_mostro;
+		
+		//Imposto tesoro come numero random  
+		pun->tesoro=rand()%250;
+		
+		//Imposto che la terra successiva di p non abbia nessun valore
+		pun->terra_successiva = NULL;
+		
+		//Assegno ad ultima terra l'indirizzo di pun
+		ultima_terra=pun;
+		
+	}
+	
+	return(ultima_terra);
+	
 }
 
 //Funzione di cancellazione del terreno creato
-struct Terra *del_terra(struct Terra* p)
+struct sTerra *del_terra(struct sTerra* p)
 {// *del_terra() - OPEN
-
-	printf("\n test del_terra funzionante");
-	return(0);
+	
+	/* if(ultima_terra==0){
+		
+		printf("\n Non hai inserito nessun terreno ancora...")
+		
+	}
+	else{
+		
+		//Libera la memoria dell'ultimo terreno avuto
+		free(ptr)
+		
+		//Imposta che l'ultimo terreno corrisponda 
+		ultima_terra=;
+		
+	}
+	*/
+	return(ultima_terra); 
+	
+	
 }
 
 //Funzione di stampa del percorso
-void stampa_percorso(struct Terra* p)
+void stampa_percorso(struct sTerra* p)
 {// stampa_percorso() - OPEN
 	
+	//Inizializzo il contatore delle terre visibile solo in stampa_percorso
 	int contaTerra=1;
-	p=percorso;
 	
-	//Stampa "nessuna Terra"
+	//Stampa nel caso in cui non sia presente nessuna terra
 	if (p == NULL){
-
-		system("clear"); // PULISCE LO SCHERMO
+		
+		system("clear");
 		
 		printf("Stampo il percorso");
 		printf ("\n\n ---Nessun terreno presente---\n");
 		
 		printf("\n - Premi ENTER per continuare -");
 		while(getchar()!='\n');
-		getchar();
-	}
-	//Stampa Terre dalla prima in poi
-	while(p!=NULL){
-		printf("\n\n  Terra nr. %d ha :", contaTerra);
-		//printf("\n Tipo terra > %d", (int)p->tipo_terra);
-		//printf("\n Tipo mostro > %d", (int)p->tipo_mostro);
-		//printf("\n Tesoro = %d", (int)p->tesoro);
+			getchar();
 		
+	}
+	
+	//Stampa delle terre dalla prima in poi
+	while(p != NULL){
+		
+		system("clear");
+		
+		printf("\n Terra nr. %d ha :", contaTerra);
+		printf("\n Tipo terra > %d", p->tipo_terra);
+		printf("\n Tipo mostro > %d", p->tipo_mostro);
+		printf("\n Tesoro = %d\n\n", p->tesoro);
+		
+		//Leggo l'elemento successivo
 		p=p->terra_successiva;
 		
+		//Contatore numero terre incrementato di 1
 		contaTerra++;
 		
 		printf("\n - Premi ENTER per continuare -");
 		while(getchar()!='\n');
-		getchar();
+			getchar();
+		
 	}
+	
 }
 
 //Funzione chiusura del percorso
-struct Terra *chiudi_percorso(struct Terra* p)
+void chiudi_percorso()
 {// *chiudi_percorso() - OPEN
-	
-	printf("\n test chiudi_percorso funzionante\n\n");
+
 	checkExit=1;
-	return(0);
+	
 }
 
 
 //-------------FUNZIONI SU OBERON-------------
 
 //Funzione di avanzamento di Oberon
-struct Oberon *avanza(struct Oberon* l)
+struct sOberon *avanza(struct sOberon* l)
 {// *avanza() - OPEN
+
 	printf("\n test avanza funzionante");
+	return(0);
+	
 }
 
 //Funzione della prendita del tesoro
-struct Oberon *prendi_tesoro(struct Oberon* l)
+struct sOberon *prendi_tesoro(struct sOberon* l)
 {// *prendi_tesoro() - OPEN
+
 	printf("\n test prendi_tesoro funzionante");
+	return(0);
+	
 }
 
 //Funzione usa pozione su Oberon
-struct Oberon *usa_pozione(struct Oberon* l)
+struct sOberon *usa_pozione(struct sOberon* l)
 {// *usa_pozione() - OPEN
+
 	printf("\n test usa_pozione funzionante");
+	return(0);
+	
 }
 
 //Funzione del combattimento
-struct Oberon *combatti(struct Oberon* l)
+struct sOberon *combatti(struct sOberon* l)
 {// *combatti() - OPEN
+
 	printf("\n test combatti funzionante");
+	return(0);
+	
 }
 
 //Funzione di distruzione terra
-struct Terra *distruggi_terra(struct Terra* p, struct Oberon* l)
+struct sTerra *distruggi_terra(struct sTerra* p, struct sOberon* l)
 {// *distruggi_terra() - OPEN
+
 	printf("\n test ditruggi_terra funzionante");
+	return(0);
+	
 }
